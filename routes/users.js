12fad4a0c554ba9bcde28cd3c9e8264af8f0c5a2
Zohaib Ali <www.zohaib.0335@gmail.com>
2025-03-27 16:41:55 -0700

    GET User by ID

--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,45 @@
 import express from "express";
+import { v4 as uuidv4 } from "uuid";
 
 const router = express.Router();
 
 const users = [
-  {
-    firstname: "Droid",
-    lastname: "GameZZ",
-    age: 24,
-  },
-  {
-    firstname: "Zohaib",
-    lastname: "Ali",
-    age: 13,
-  },
+  // {
+  //   firstname: "Droid",
+  //   lastname: "GameZZ",
+  //   age: 24,
+  // },
+  // {
+  //   firstname: "Zohaib",
+  //   lastname: "Ali",
+  //   age: 13,
+  // },
 ];
 
+// Get all users
 router.get("/", (req, res) => {
   console.log(users);
   // res.send("Hello");
   res.json({ length: users.length, users: users });
 });
 
+// Create a user
 router.post("/", (req, res) => {
-  users.push(req.body);
-  res.json({ message: "success", user: req.body });
+  const user = req.body;
+  user.id = uuidv4();
+  users.push(user);
+  res.json({ message: "success", user: user });
+});
+
+// Get user by ID
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const foundUser = users.find((user) => {
+    if (user.id === id) {
+      return user;
+    }
+  });
+  res.json({ user: foundUser });
 });
 
 export default router;
